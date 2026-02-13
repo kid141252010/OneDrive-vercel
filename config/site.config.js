45ccbb9caedfee26db10998f0de2ac70c0485b89
kid141252010 <kid@nanhaijue1412.onmicrosoft.com>
2026-02-13 17:14:08 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/android-chrome-192x192.png',
+  icon: '/android-chrome-512x512.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
