4d9f08f54d5dd4271cef22b4b461041e3eb9669d
kid141252010 <250306205+kid141252010@users.noreply.github.com>
2026-03-03 17:58:32 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/BL', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/BL', '/手机备份', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:nanheijue@gmail.com',
